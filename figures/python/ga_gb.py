243bebbe0c24a658cc4bae70e5ce609e25151af6
ole-peters <ole.peters@zonlab.com>
2019-05-29 14:49:21 +0100

    added ga-gb figure and text
    could be expanded. Maybe the example from the tweet is easier to understand?

--- figures/python/ga_gb.py
+++ figures/python/ga_gb.py
@@ -11,9 +11,9 @@ t0=0
 ta=1
 tb=2
 x0=np.arange(1,10000,1)
-Dxa=10
-Dxb=25
-r=0.0
+Dxa=500
+Dxb=1200
+r=0.03
 
 ga=1/(ta-t0)*np.log(1+(Dxa/(x0*np.exp(r*(ta-t0)))))+r
 gb=1/(tb-t0)*np.log(1+(Dxb/(x0*np.exp(r*(tb-t0)))))+r
@@ -24,10 +24,12 @@ plt.plot(x0,ga-gb,linestyle='-',color='blue')
 #plt.plot(x0,gb,linestyle='-',color='red')
 plt.rc('xtick',labelsize=16)
 plt.rc('ytick',labelsize=16)
-plt.xlabel('$x(t_0)$',fontsize=16)
-plt.ylabel('$g_a-g_b$',fontsize=16)
 
-plt.xlim(0,500)
-plt.ylim(-.03,.03)
+plt.plot([0, 10000], [0, 0], 'k--', lw=1)
+plt.xlabel('$x(t_0)$ (dollars)',fontsize=16)
+plt.ylabel('$g_a-g_b$ (per annum)',fontsize=16)
+
+plt.xlim(0,10000)
+plt.ylim(-.02,.02)
 plt.savefig("./../ga_gb.pdf", bbox_inches='tight')
 plt.show()
