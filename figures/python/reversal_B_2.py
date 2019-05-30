59bd15959810c6245464f0e9ea537179495f37d7
ole-peters <ole.peters@zonlab.com>
2019-05-30 10:35:38 +0100

    harmonized parameters for figures in section B
    we can still prettify figures, but ok as a draft.

--- figures/python/reversal_B_2.py
+++ figures/python/reversal_B_2.py
@@ -17,39 +17,41 @@ fig1, ax1 = plt.subplots()
 t0=0
 ta=1
 tb=2
-x0=20
-Dxa=10
-Dxb=25
-tPR=(Dxa*tb-Dxb*ta)/(Dxa-Dxb)
+x0=2277
+Dxa=1000
+Dxb=2500
+r=.03
+
+
+xta=x0*np.exp(r*(ta-t0))+Dxa
+xtb=x0*np.exp(r*(tb-t0))+Dxb
+
 
 #Payouts
-plt.plot([ta, ta], [x0, x0+Dxa], 'b-', lw=3)
-plt.plot([tb, tb], [x0, x0+Dxb], 'r-', lw=3)
+plt.plot([ta, ta], [x0, xta], 'b-', lw=3)
+plt.plot([tb, tb], [x0, xtb], 'r-', lw=3)
 
 #wealth levels
 plt.plot([-.3, 2.5], [x0, x0], 'k:', lw=1)
-plt.plot([-.3, 2.5], [x0+Dxa, x0+Dxa], 'k:', lw=1)
-plt.plot([-.3, 2.5], [x0+Dxb, x0+Dxb], 'k:', lw=1)
+plt.plot([-.3, 2.5], [xta, xta], 'k:', lw=1)
+plt.plot([-.3, 2.5], [xtb, xtb], 'k:', lw=1)
 
-# slopes
-plt.plot([t0,ta], [x0, x0+Dxa], 'b:', lw=2)
-plt.plot([t0,tb], [x0, x0+Dxb], 'r:', lw=2)
+#slopes
+plt.plot([t0,ta], [x0, xta], 'b:', lw=2)
+plt.plot([t0,tb], [x0, xtb], 'r:', lw=2)
 
 plt.rc('xtick',labelsize=16)
 plt.rc('ytick',labelsize=16)
 plt.gca().spines['right'].set_color('none')
 plt.gca().spines['top'].set_color('none')
-
-print(plt.yticks())
-
+#plt.xlabel('$t$',fontsize=16)
+#plt.ylabel('Payment',fontsize=16)
 plt.yscale('log')
 ax1.yaxis.set_major_formatter(NullFormatter())
 ax1.yaxis.set_minor_formatter(NullFormatter())
-print(plt.yticks())
 plt.xticks([t0,ta,tb], ['$t_0$','$t_a$','$t_b$'], rotation=0)
-plt.yticks([x0,x0+Dxa,x0+Dxb], ['$x(t_0)=x(t_0)^{PR}$','$x(t_0)+\Delta x_a$','$x(t_0)+\Delta x_b$'], rotation=0)
-print(plt.yticks())
+plt.yticks([x0,x0+Dxa,x0+Dxb], ['$x(t_0)=x(t_0)^{PR}$','$x(t_a)$','$x(t_b)$'], rotation=0)
 plt.xlim(-0.2,2.5)
-plt.ylim(x0,x0+Dxb+10)
+plt.ylim(x0,xtb*1.03)
 plt.savefig("./../reversal_B_2.pdf", bbox_inches='tight')
 plt.show()
